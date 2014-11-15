6c5b40d8393a4b43b4bce9517d982a6c73b978b8
bguiz <brendan.graetz@gmail.com>
2014-11-16 01:20:55 +1100

    =BG= add ability to have multiple images; also image dimensions can be configured

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var lpaData = require('./data/leanpub-affiliate.json');
 
+var len = lpaData.length;
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 function pickAd() {
-    var idx = getRandomInt(0, lpaData.length -1);
+    var idx = getRandomInt(0, len -1);
     return lpaData[idx];
 }
 
@@ -19,20 +20,33 @@ module.exports.book = {
 
 module.exports.hooks = {
     "page:before": function(page) {
-        console.log('page:before');
         page.content += '\n\n<div class="gitbook-leanpub-affiliate"></div>\n\n';
         return page;
     },
     "page:after": function(page) {
-        console.log('page:after');
         var config = this.options.pluginsConfig.leanpubAffiliate || {};
         config.queryString = config.queryString || '?a=27hVMyWVn46xaZCi6E563X&subID=gitbookPlugin';
-        var ad = pickAd();
-        console.log('page:after', ad);
-        out = '\n\n<a href="'+ad.url+config.queryString+'" class="gitbook-leanpub-affiliate">';
-        out += '<img src="'+ad.image+'" />';
-        out += '<span>'+ad.title+' by '+ad.author_string+'</span>';
-        out += '\n</a>\n\n';
+        config.image = config.image || {
+            display: true,
+            width: 68,
+            height: 100,
+        };
+        config.count = config.count || 3;
+        out = '\n\n<br /><hr />';
+        for (var c = 0; c < config.count; ++c) {
+            var ad = pickAd();
+            out += '<a href="'+ad.url+config.queryString+'" class="gitbook-leanpub-affiliate">';
+            if (config.image.display) {
+                out += '<img width="'+config.image.width+'" height="'+config.image.height+'" src="'+ad.image+'" class="gitbook-leanpub-affiliate-cover" />';
+            }
+            out +=
+                '<span class="gitbook-leanpub-affiliate-text">'+
+                    '<div>'+ad.title+'</div>'+
+                    '<div>by '+ad.author_string+'</div>'+
+                '</span></a>\n';
+        }
+
+        out += '<hr />\n\n';
 
         page.content = page.content.replace('<div class="gitbook-leanpub-affiliate"></div>', out);
         return page;
