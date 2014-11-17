5cd0d37361e793f1540cfbafe9e5426dcf2043b0
bguiz <brendan.graetz@gmail.com>
2014-11-17 21:00:21 +1100

    =BG= links are dynamically loaded at run time instead of during page generation; plus move some styles to css instead of html inline

--- index.js
+++ index.js
@@ -8,22 +8,54 @@ function pickAd() {
     var idx = getRandomInt(0, len -1);
     return lpaData[idx];
 }
+function pickAds(num) {
+    var ads = [];
+    if (num >= len) {
+        ads = lpaData;
+    }
+    else {
+        // Use probability based filtering to get most of the way there,
+        // Then add or cull a few more to level off to the required number
+        // This is not ideal, because the end result may contain a small number of repetitions
+        var probability = num / len;
+        ads = lpaData.filter(function(ad) {
+            return Math.random() < probability;
+        });
+        while (ads.length < num) {
+            ads.push(pickAd());
+        }
+        while (ads.length > num) {
+            //TODO this bit could be more efficient
+            ads.splice(getRandomInt(0, ads.length), 1);
+        }
+    }
+    ads = ads.map(function(ad) {
+        //cull properties that will not be used for display
+        return {
+            url: ad.url,
+            image: ad.image,
+            title: ad.title,
+            author_string: ad.author_string,
+        };
+    });
+    return ads;
+}
+var pageCount = 0;
 
 module.exports.book = {
     assets: "./book",
     js: [
-        "plugin.js"
+        "plugin.js",
+    ],
+    css: [
+        "plugin.css",
     ],
     html: {
     },
 };
 
 module.exports.hooks = {
-    "page:before": function(page) {
-        page.content += '\n\n<div class="gitbook-leanpub-affiliate"></div>\n\n';
-        return page;
-    },
-    "page:after": function(page) {
+    init: function() {
         var config = this.options.pluginsConfig.leanpubAffiliate || {};
         config.queryString = config.queryString || '?a=27hVMyWVn46xaZCi6E563X&subID=gitbookPlugin';
         if (typeof config.queryString !== 'string') {
@@ -37,27 +69,23 @@ module.exports.hooks = {
             height: 100,
         };
         config.count = config.count || 3;
-        var widthPercent = Math.floor(90 / config.count);
-        var widthPixels = Math.floor(config.image.width);
-        out = '\n\n<br /><hr />';
-        for (var c = 0; c < config.count; ++c) {
-            var ad = pickAd();
-            out += '<a href="'+ad.url+config.queryString+'" class="gitbook-leanpub-affiliate" '+
-                'style="display: inline-block; max-width: '+widthPercent+'%; min-width: '+widthPixels+'px; vertical-align: top;">';
-            if (config.image.display) {
-                out += '<img width="'+config.image.width+'" height="'+config.image.height+
-                    '" src="'+ad.image+'" class="gitbook-leanpub-affiliate-cover" /><br />';
-            }
-            out +=
-                '<span class="gitbook-leanpub-affiliate-text">'+
-                    '<span>'+ad.title+'</span><br />'+
-                    '<span>by '+ad.author_string+'</span>'+
-                '</span></a>\n';
-        }
+        config.total = Math.max(config.total || config.count, config.count * 2);
+        config.ads = pickAds(config.total);
+        console.log('Leanpub affiliate links count:', config.ads.length);
+        this.options.pluginsConfig.leanpubAffiliate = config;
+    },
 
-        out += '<hr />\n\n';
+    finish: function() {
+        console.log('Total pages with Leanpub affiliate links:', pageCount);
+    },
 
-        page.content = page.content.replace('<div class="gitbook-leanpub-affiliate"></div>', out);
+    "page:before": function(page) {
+        ++pageCount;
+        page.content += '\n\n<div class="gitbook-leanpub-affiliate"></div>\n\n';
+        return page;
+    },
+    "page:after": function(page) {
+        //Do nothing
         return page;
     },
 };
